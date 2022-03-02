ca1bdd778aca3e6d19db0ac4d4edd30f31778cb6
elzup <guild0105@gmail.com>
2022-03-03 08:10:56 +0900

    refactor minify

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,2 @@
-const directList = [
-  'undefined',
-  'boolean',
-  'number',
-  'bigint',
-  'string',
-  'symbol',
-  'function',
-]
-
-export const kindof = (v: unknown) => {
-  const tv = typeof v
-
-  if (directList.includes(tv)) return tv
-  switch (typeof v) {
-    case 'object':
-      if (v === null) return 'null'
-      if (Array.isArray(v)) return 'array'
-      return 'object'
-    default:
-      return 'unknown'
-  }
-}
+export const kindof = (v: unknown) =>
+  v === null ? 'null' : Array.isArray(v) ? 'array' : typeof v
